3f2c74ed47691636dd69d7f0bb526405861d613f
Martin Westergaard Lassen <mwl@trifork.com>
2012-03-08 14:15:26 +0100

    container port configuration

---
+++ integration-test/src/main/java/dk/bemyndigelsesregister/integrationtest/TestUtil.java
@@ -0,0 +1,8 @@
+package dk.bemyndigelsesregister.integrationtest;
+
+public class TestUtil {
+
+    public static String urlPrefix() {
+        return System.getProperty("functionaltest.urlprefix");
+    }
+}
--- integration-test/src/test/java/penetration/HealthServiceITCase.java
+++ integration-test/src/test/java/penetration/HealthServiceITCase.java
@@ -5,19 +5,20 @@ import org.junit.Test;
 
 import java.net.*;
 
+import static dk.bemyndigelsesregister.integrationtest.TestUtil.urlPrefix;
 import static org.junit.Assert.*;
 
 public class HealthServiceITCase {
 
     @Test
     public void canAccessBemyndigelsesserviceHealth() throws Exception {
-        URLConnection urlConnection = new URL("http://localhost:8080/bemyndigelsesservice/health").openConnection();
+        URLConnection urlConnection = new URL(urlPrefix() +  "/bemyndigelsesservice/health").openConnection();
         assertEquals("OK", IOUtils.toString(urlConnection.getInputStream()));
     }
 
     @Test
     public void canAccessStamdataserviceHealth() throws Exception {
-        URLConnection urlConnection = new URL("http://localhost:8080/stamdataservice/health").openConnection();
+        URLConnection urlConnection = new URL(urlPrefix() + "/stamdataservice/health").openConnection();
         assertEquals("OK", IOUtils.toString(urlConnection.getInputStream()));
     }
 }
