8b49397c36dcfeb12a8125af8363e333345aaac0
Martin Westergaard Lassen <martin@mwl.dk>
2012-03-11 22:17:23 +0100

    jboss 6 degrade

--- bemyndigelsesservice/src/main/java/dk/bemyndigelsesregister/bemyndigelsesservice/config/EmbeddedConfiguration.java
+++ bemyndigelsesservice/src/main/java/dk/bemyndigelsesregister/bemyndigelsesservice/config/EmbeddedConfiguration.java
@@ -1,22 +1,19 @@
 package dk.bemyndigelsesregister.bemyndigelsesservice.config;
 
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
-import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
 
 import javax.sql.DataSource;
 
 //@Configuration
 @Profile("embedded")
 public class EmbeddedConfiguration {
-    private final Log logger = LogFactory.getLog(getClass());
+    //private final Log logger = LogFactory.getLog(getClass());
 
     @Bean
     public DataSource dataSource() {
-        logger.info("Creating development database");
-        return new EmbeddedDatabaseBuilder().build();
+        //logger.info("Creating development database");
+        //return new EmbeddedDatabaseBuilder().build();
+        return null;
     }
 }
