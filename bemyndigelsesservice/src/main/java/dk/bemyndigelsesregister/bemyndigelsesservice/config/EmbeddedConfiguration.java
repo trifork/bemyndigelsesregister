681313d2c72c69054c339711258ea22646b40850
Martin Westergaard Lassen <mwl@trifork.com>
2012-03-12 13:37:31 +0100

    Updated Spring webmvc to 3.1.1 and added spring-jdbc and hsqldb

--- bemyndigelsesservice/src/main/java/dk/bemyndigelsesregister/bemyndigelsesservice/config/EmbeddedConfiguration.java
+++ bemyndigelsesservice/src/main/java/dk/bemyndigelsesregister/bemyndigelsesservice/config/EmbeddedConfiguration.java
@@ -1,18 +1,21 @@
 package dk.bemyndigelsesregister.bemyndigelsesservice.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
 
 import javax.sql.DataSource;
 
-//@Configuration
+@Configuration
 @Profile("embedded")
 public class EmbeddedConfiguration {
     //private final Log logger = LogFactory.getLog(getClass());
 
-    //@Bean
+    @Bean
     public DataSource dataSource() {
         //logger.info("Creating development database");
-        //return new EmbeddedDatabaseBuilder().build();
-        return null;
+        return new EmbeddedDatabaseBuilder().setType(EmbeddedDatabaseType.HSQL).build();
     }
 }
