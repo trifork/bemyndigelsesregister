89e07392aa3b7c7890a3ac7f38bf2c696bd88234
Martin Westergaard Lassen <mwl@trifork.com>
2012-03-20 10:01:54 +0100

    Removed old Authorization object

--- bemyndigelsesservice/src/main/java/dk/bemyndigelsesregister/bemyndigelsesservice/server/dao/AuthorizationDao.java
+++
@@ -1,7 +0,0 @@
-package dk.bemyndigelsesregister.bemyndigelsesservice.server.dao;
-
-import dk.bemyndigelsesregister.bemyndigelsesservice.domain.Authorization;
-
-public interface AuthorizationDao {
-    Authorization get(long id);
-}
