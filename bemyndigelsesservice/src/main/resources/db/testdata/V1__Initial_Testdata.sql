823383a67b3df102043c4cae2b1f0f2ca0a118c7
Martin Westergaard Lassen <mwl@trifork.com>
2012-05-07 15:37:13 +0200

    HentBemyndigelser should only take one of Bemyndigende or Bemyndigede

--- bemyndigelsesservice/src/main/resources/db/testdata/V1__Initial_Testdata.sql
+++ bemyndigelsesservice/src/main/resources/db/testdata/V1__Initial_Testdata.sql
@@ -72,3 +72,4 @@ INSERT INTO `system_variable` (`name`, `value`) VALUES ('testVariable', 'den god
 INSERT INTO `whitelist` (`name`, `legal_cvr`) VALUES ('test', '1');
 INSERT INTO `whitelist` (`name`, `legal_cvr`) VALUES ('test', '2');
 INSERT INTO `whitelist` (`name`, `legal_cvr`) VALUES ('BemyndigelsesService.opretAnmodningOmBemyndigelser', '25520041');
+INSERT INTO `whitelist` (`name`, `legal_cvr`) VALUES ('BemyndigelsesService.hentBemyndigelser', '25520041');
