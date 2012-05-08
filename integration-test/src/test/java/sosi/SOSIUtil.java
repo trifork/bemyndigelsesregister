175edf27dccde2ca312bb3d04b7ac8fd89834cb1
Kjeld Froberg <kfr@trifork.com>
2012-05-08 11:04:32 +0200

    added sosi system testcase

--- integration-test/src/test/java/sosi/SOSIUtil.java
+++ integration-test/src/test/java/sosi/SOSIUtil.java
@@ -11,8 +11,6 @@ import java.security.cert.X509Certificate;
 import java.util.Properties;
 
 import org.apache.commons.io.IOUtils;
-import org.junit.Ignore;
-import org.junit.Test;
 import org.w3c.dom.Document;
 
 import dk.sosi.seal.SOSIFactory;
@@ -22,6 +20,7 @@ import dk.sosi.seal.model.IDCard;
 import dk.sosi.seal.model.Request;
 import dk.sosi.seal.model.SecurityTokenRequest;
 import dk.sosi.seal.model.SignatureUtil;
+import dk.sosi.seal.model.SystemIDCard;
 import dk.sosi.seal.model.UserIDCard;
 import dk.sosi.seal.model.UserInfo;
 import dk.sosi.seal.model.constants.SubjectIdentifierTypeValues;
@@ -32,16 +31,14 @@ import dk.sosi.seal.xml.XmlUtil;
 public class SOSIUtil {
 
     public static Document getIdCard() throws Exception {
-        Properties props = SignatureUtil.setupCryptoProviderForJVM();
 
+        Properties props = SignatureUtil.setupCryptoProviderForJVM();
         props.setProperty(SOSIFactory.PROPERTYNAME_SOSI_VALIDATE_ENHANCED, "false");
         props.setProperty(SOSIFactory.PROPERTYNAME_SOSI_VALIDATE, "false");
 
         SOSIFederation federation = new SOSIFederation(props);
         ClasspathCredentialVault vault = new ClasspathCredentialVault(props, "validMocesVault.jks", "Test1234");
         SOSIFactory factory = new SOSIFactory(federation, vault, props);
-
-        SecurityTokenRequest stRequest = factory.createNewSecurityTokenRequest();
 
         X509Certificate certificate = vault.getSystemCredentialPair().getCertificate();
 
@@ -50,6 +47,54 @@ public class SOSIUtil {
 		UserIDCard card = factory.createNewUserIDCard("SOSITEST", userInfo, careProvider, 
 				AuthenticationLevel.MOCES_TRUSTED_USER, "", "", certificate, "TEST");
 
+		SecurityTokenRequest stRequest = factory.createNewSecurityTokenRequest();
+        stRequest.setIDCard(card);
+
+        Document requestDocument = stRequest.serialize2DOMDocument();
+
+        byte[] bytesForSigning = stRequest.getIDCard().getBytesForSigning(requestDocument);
+
+        Signature jceSign = Signature.getInstance("SHA1withRSA", SignatureUtil.getCryptoProvider(props,
+                SOSIFactory.PROPERTYNAME_SOSI_CRYPTOPROVIDER_SHA1WITHRSA));
+        PrivateKey key = vault.getSystemCredentialPair().getPrivateKey();
+        jceSign.initSign(key);
+        jceSign.update(bytesForSigning);
+        String signature = XmlUtil.toBase64(jceSign.sign());
+        
+        X509Certificate certificate2 = vault.getSystemCredentialPair().getCertificate();
+        stRequest.getIDCard().injectSignature(signature, certificate2);
+
+        String xml = XmlUtil.node2String(stRequest.serialize2DOMDocument(), false, true);
+        String resXml = sendRequest("http://pan.certifikat.dk/sts/services/SecurityTokenService", "", xml, false);
+
+        SecurityTokenRequest res = factory.deserializeSecurityTokenRequest(resXml);
+        IDCard idCard = res.getIDCard();
+
+        Request sreq = factory.createNewRequest(false, null);
+        sreq.setIDCard(idCard);
+        Document reqdoc = sreq.serialize2DOMDocument();
+
+        String xmlRequest = XmlUtil.node2String(reqdoc, false, true);
+        System.out.println(xmlRequest);
+        return reqdoc;
+    }
+
+    public static Document getSystemIdCard() throws Exception {
+
+        Properties props = SignatureUtil.setupCryptoProviderForJVM();
+        props.setProperty(SOSIFactory.PROPERTYNAME_SOSI_VALIDATE_ENHANCED, "false");
+        props.setProperty(SOSIFactory.PROPERTYNAME_SOSI_VALIDATE, "false");
+        
+        SOSIFederation federation = new SOSIFederation(props);
+        ClasspathCredentialVault vault = new ClasspathCredentialVault(props, "test_voces1.jks", "Test1234");
+        SOSIFactory factory = new SOSIFactory(federation, vault, props);
+
+        X509Certificate certificate = vault.getSystemCredentialPair().getCertificate();
+
+        CareProvider careProvider = new CareProvider(SubjectIdentifierTypeValues.CVR_NUMBER, "25520041", "test");
+        SystemIDCard card = factory.createNewSystemIDCard("SOSITEST", careProvider, AuthenticationLevel.VOCES_TRUSTED_SYSTEM, "", "", certificate, "TEST");
+
+        SecurityTokenRequest stRequest = factory.createNewSecurityTokenRequest();
         stRequest.setIDCard(card);
 
         Document requestDocument = stRequest.serialize2DOMDocument();
@@ -110,5 +155,14 @@ public class SOSIUtil {
         uc.disconnect();
 
         return res;
+    }
+    
+    public static void main(String[] args) {
+        try {
+            getSystemIdCard();
+        } catch (Exception e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
     }
 }
