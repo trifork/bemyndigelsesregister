32a4c86e9a8df714b1d74e0ca727c8b212bc9963
Martin Westergaard Lassen <mwl@trifork.com>
2012-05-22 08:13:40 +0200

    DelegerbarRettighed dao

---
+++ bemyndigelsesservice/src/main/java/dk/bemyndigelsesregister/bemyndigelsesservice/server/dao/ebean/DelegerbarRettighedDaoEbean.java
@@ -0,0 +1,21 @@
+package dk.bemyndigelsesregister.bemyndigelsesservice.server.dao.ebean;
+
+import dk.bemyndigelsesregister.bemyndigelsesservice.domain.DelegerbarRettighed;
+import dk.bemyndigelsesregister.bemyndigelsesservice.domain.Domaene;
+import dk.bemyndigelsesregister.bemyndigelsesservice.domain.LinkedSystem;
+import dk.bemyndigelsesregister.bemyndigelsesservice.server.dao.DelegerbarRettighedDao;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public class DelegerbarRettighedDaoEbean extends SupportDao<DelegerbarRettighed> implements DelegerbarRettighedDao {
+    public DelegerbarRettighedDaoEbean() {
+        super(DelegerbarRettighed.class);
+    }
+
+    @Override
+    public List<DelegerbarRettighed> findBy(Domaene domaene, LinkedSystem linkedSystem) {
+        return query().where().eq("domaene", domaene).eq("system", linkedSystem).findList();
+    }
+}
