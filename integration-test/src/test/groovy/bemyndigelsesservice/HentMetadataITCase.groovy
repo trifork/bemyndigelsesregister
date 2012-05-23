96619d4b55ba43d13d771e28fb66de299b27aa0e
Martin Westergaard Lassen <mwl@trifork.com>
2012-05-23 15:43:46 +0200

    Integrationstest af HenMetadata

---
+++ integration-test/src/test/groovy/bemyndigelsesservice/HentMetadataITCase.groovy
@@ -0,0 +1,19 @@
+package bemyndigelsesservice
+
+import org.junit.Test
+import shared.WebServiceSupport
+
+class HentMetadataITCase extends WebServiceSupport {
+
+    @Test
+    public void willAllowWhitelistedSystemsToRead() throws Exception {
+        def response = send("hentMetadata") {
+            "bms:HentMetadataRequest" {
+                "Domaene"('trifork')
+                "System"('Trifork test system')
+            }
+        }
+        assert response
+        assert 1 == response.HentMetadataResponse.Arbejdsfunktioner.size()
+    }
+}
