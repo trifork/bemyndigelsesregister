b524bd138c4cb1a3cea2713c695dc5e5acc74a54
Kari Rye Schougaard <krs@trifork.com>
2016-04-05 09:27:11 +0200

    Initdb metadata for FMK, DDV and TAS

--- bemyndigelsesservice/src/main/resources/db/manual/trifork_data.sql
+++ bemyndigelsesservice/src/main/resources/db/manual/trifork_data.sql
@@ -6,6 +6,8 @@ REPLACE INTO linked_system (domaene_id, kode, beskrivelse, sidst_modificeret, si
 
 REPLACE INTO linked_system (domaene_id, kode, beskrivelse, sidst_modificeret, sidst_modificeret_af) VALUES (1, 'FMK', 'Det Fælles Medicinkort', '2012-11-30 10:00:00', 'frj@trifork.com');
 
+REPLACE INTO linked_system (domaene_id, kode, beskrivelse, sidst_modificeret, sidst_modificeret_af) VALUES (1, 'TAS', 'Tilskudansøgningsservicen', '2016-04-04 10:00:00', 'krs@trifork.com');
+
 
 /* Whitelist 25520041 - dette cvr kan hente metadata, indlaese metadata og oprette bemyndigelser (OGSAA godkendte bemyndigelser) */
 REPLACE INTO `whitelist` (`name`, `legal_cvr`) VALUES ('bemyndigelsesservice.indlaesMetadata', '25520041');
