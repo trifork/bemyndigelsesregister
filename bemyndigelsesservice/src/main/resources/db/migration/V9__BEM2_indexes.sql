2303b9af0903b43d01565f1affad231e77e5f5d8
Ole Bak Jensen <obj@trifork.com>
2016-04-13 09:43:39 +0200

    Indexes for BEM2 tables

---
+++ bemyndigelsesservice/src/main/resources/db/migration/V9__BEM2_indexes.sql
@@ -0,0 +1,5 @@
+ALTER TABLE `bemyndigelse`.`bemyndigelse20`
+ADD INDEX `ix_bemyndigende_cpr` (`bemyndigende_cpr` ASC),
+ADD INDEX `ix_bemyndigede_cpr` (`bemyndigede_cpr` ASC),
+ADD INDEX `ix_sidst_modificeret` (`sidst_modificeret` ASC),
+ADD INDEX `ix_system_gyldig_til` (`linked_system_kode` ASC, `gyldig_til` ASC);
