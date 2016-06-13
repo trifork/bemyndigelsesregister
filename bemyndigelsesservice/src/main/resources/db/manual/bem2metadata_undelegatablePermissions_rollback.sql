7e01e535a8168930a0b8814d2d34e65bd4284abc
Kari Rye Schougaard <krs@trifork.com>
2016-06-13 11:20:54 +0200

    rollback to undelegatable permission sql

---
+++ bemyndigelsesservice/src/main/resources/db/manual/bem2metadata_undelegatablePermissions_rollback.sql
@@ -0,0 +1,3 @@
+UPDATE `bemyndigelse`.`delegatable_permission` SET `delegerbar` = 1 WHERE `arbejdsfunktion_id`=42 AND `rettighedskode_id`=21;
+UPDATE `bemyndigelse`.`delegatable_permission` SET `delegerbar` = 1 WHERE `arbejdsfunktion_id`=43 AND `rettighedskode_id`=21;
+
