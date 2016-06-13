ff2b2193bb3e1691c9a2591cbe52eac5d6ba1b5f
Kari Rye Schougaard <krs@trifork.com>
2016-06-13 11:17:41 +0200

    Manuel metadata opdatering ikke-delegerbare rettigheder

---
+++ bemyndigelsesservice/src/main/resources/db/manual/bem2metadata_undelegatablePermissions.sql
@@ -0,0 +1,3 @@
+UPDATE `bemyndigelse`.`delegatable_permission` SET `delegerbar` = 0 WHERE `arbejdsfunktion_id`=42 AND `rettighedskode_id`=21;
+UPDATE `bemyndigelse`.`delegatable_permission` SET `delegerbar` = 0 WHERE `arbejdsfunktion_id`=43 AND `rettighedskode_id`=21;
+
