b4eec4c6ca96a2cd50f423a03d9dfb8f705b35dd
Kari Rye Schougaard <krs@trifork.com>
2016-06-13 11:09:15 +0200

    system long name manual metadataupdate rollback

--- bemyndigelsesservice/src/main/resources/db/manual/bem2metadata_systemLongName.sql
+++ bemyndigelsesservice/src/main/resources/db/manual/bem2metadata_systemLongName.sql
@@ -1,4 +1,4 @@
-UPDATE linked_system SET beskrivelse = 'Det Fælles Medicinkort' where kode = 'FMK';
-UPDATE linked_system SET beskrivelse = 'Det Danske Vaccinationsregister' where kode = 'DDV';
-UPDATE linked_system SET beskrivelse = 'Tilskudsansøgningsservicen' where kode = 'TAS';
+UPDATE `bemyndigelse`.`linked_system` SET beskrivelse = 'Det Fælles Medicinkort' where kode = 'FMK';
+UPDATE `bemyndigelse`.`linked_system` SET beskrivelse = 'Det Danske Vaccinationsregister' where kode = 'DDV';
+UPDATE `bemyndigelse`.`linked_system` SET beskrivelse = 'Tilskudsansøgningsservicen' where kode = 'TAS';
 
