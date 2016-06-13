12f0a37e471ab5ba73581ed9b0f63670cad8ed02
Ole Bak Jensen <obj@trifork.com>
2016-06-13 11:17:21 +0200

    Bugfix til rollback-sql

--- bemyndigelsesservice/src/main/resources/db/manual/bem2metadata_asteriskPermission_rollback.sql
+++ bemyndigelsesservice/src/main/resources/db/manual/bem2metadata_asteriskPermission_rollback.sql
@@ -1,6 +1,6 @@
-DELETE FROM `bemyndigelse`.`delegerbar_rettighed` WHERE `bemyndigelse`.`rettighedskode_id`=53;
-DELETE FROM `bemyndigelse`.`delegerbar_rettighed` WHERE `bemyndigelse`.`rettighedskode_id`=52;
-DELETE FROM `bemyndigelse`.`delegerbar_rettighed` WHERE `bemyndigelse`.`rettighedskode_id`=51;
-DELETE FROM `bemyndigelse`.`rettighed` WHERE `bemyndigelse`.`id`=51;
-DELETE FROM `bemyndigelse`.`rettighed` WHERE `bemyndigelse`.`id`=52;
-DELETE FROM `bemyndigelse`.`rettighed` WHERE `bemyndigelse`.`id`=53;
+DELETE FROM `bemyndigelse`.`delegerbar_rettighed` WHERE `rettighedskode_id`=53;
+DELETE FROM `bemyndigelse`.`delegerbar_rettighed` WHERE `rettighedskode_id`=52;
+DELETE FROM `bemyndigelse`.`delegerbar_rettighed` WHERE `rettighedskode_id`=51;
+DELETE FROM `bemyndigelse`.`rettighed` WHERE `id`=51;
+DELETE FROM `bemyndigelse`.`rettighed` WHERE `id`=52;
+DELETE FROM `bemyndigelse`.`rettighed` WHERE `id`=53;
