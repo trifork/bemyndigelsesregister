a8be80da773c2344cdd536c10f77be140f83fe01
Kari Rye Schougaard <krs@trifork.com>
2016-06-13 13:07:20 +0200

    Updata metadata FMK indlaeg og ajourfoer

---
+++ bemyndigelsesservice/src/main/resources/db/manual/metadata_update_ajourfoering_indlaeggelse.sql
@@ -0,0 +1,4 @@
+UPDATE `bemyndigelse`.`rettighed` SET beskrivelse = 'Indlæg/udskriv via FMK-snitflade 1.4.0' where kode = 'Suspendering';
+UPDATE `bemyndigelse`.`rettighed` SET beskrivelse = 'Sæt markering for ajourføring' where kode = 'Afstemning';
+UPDATE `bemyndigelse`.`rettighed` SET beskrivelse = 'Opret tilknytning til medicinadministration og Indlæg/udskriv fra FMK 1.4.2 og 1.4.4' where kode = 'Tilknytning';
+
