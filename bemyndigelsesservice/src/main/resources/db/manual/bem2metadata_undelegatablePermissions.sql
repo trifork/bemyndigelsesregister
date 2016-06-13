41cd60843d3f9832f8f19a41f4e121862b9c81ab
Ole Bak Jensen <obj@trifork.com>
2016-06-13 11:36:58 +0200

    Bugfix til sql

--- bemyndigelsesservice/src/main/resources/db/manual/bem2metadata_undelegatablePermissions.sql
+++ bemyndigelsesservice/src/main/resources/db/manual/bem2metadata_undelegatablePermissions.sql
@@ -1,4 +1,4 @@
-UPDATE `bemyndigelse`.`delegerbar_rettighed` (`arbejdsfunktion_id`,`rettighedskode_id`,`sidst_modificeret`,`sidst_modificeret_af`,`delegerbar`)
+INSERT INTO `bemyndigelse`.`delegerbar_rettighed` (`arbejdsfunktion_id`,`rettighedskode_id`,`sidst_modificeret`,`sidst_modificeret_af`,`delegerbar`)
 VALUES
 (42,21,'2016-06-13 11:30:00','FMK-8853',0),
 (43,21,'2016-06-13 11:30:00','FMK-8853',0);
