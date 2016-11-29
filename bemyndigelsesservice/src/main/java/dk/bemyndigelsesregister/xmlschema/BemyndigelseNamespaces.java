8813c6aec7295f5c8fad1e439be98028411c54a5
Ole Bak Jensen <obj@trifork.com>
2016-11-29 14:58:49 +0100

    Added inline schema generator lifted from FMK-schemas project

---
+++ bemyndigelsesservice/src/main/java/dk/bemyndigelsesregister/xmlschema/BemyndigelseNamespaces.java
@@ -0,0 +1,16 @@
+package dk.bemyndigelsesregister.xmlschema;
+
+import com.trifork.xmlquery.Namespaces;
+
+public class BemyndigelseNamespaces {
+
+	public static Namespaces getNamespaces() {
+		Namespaces namespaces = Namespaces.getOIONamespaces();
+		namespaces.addNamespace("bms20160101", "http://nsi.dk/bemyndigelse/2016/01/01/");
+		namespaces.addNamespace("cpr", "http://rep.oio.dk/cpr.dk/xml/schemas/core/2005/03/18/");
+		namespaces.addNamespace("cvr", "http://rep.oio.dk/cvr.dk/xml/schemas/2005/03/22/");
+		namespaces.addNamespace("medcom", "http://www.medcom.dk/dgws/2006/04/dgws-1.0.xsd");
+		namespaces.addNamespace("xs", "http://www.w3.org/2001/XMLSchema");
+		return namespaces;
+	}
+}
