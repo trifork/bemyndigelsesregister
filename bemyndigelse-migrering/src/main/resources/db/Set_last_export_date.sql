b2864a3cbc49be7fb073c08da790fb0973f07a2c
Ole Bak Jensen <obj@trifork.com>
2018-02-13 15:10:06 +0100

    Deleted bemyndigelse-migrering module

--- bemyndigelse-migrering/src/main/resources/db/Set_last_export_date.sql
+++
@@ -1,2 +0,0 @@
-# After migration all delegations should be exported again, set last delegation time to zero
-UPDATE system_variable SET value=0 WHERE id=1;
--- bemyndigelse-migrering/src/main/resources/migration.bat
+++
@@ -1,1 +0,0 @@
-java -Dlog4j.configuration=file:./log4j.properties -jar migrering-2.1-SNAPSHOT.one-jar.jar all
