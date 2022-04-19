f394e6815992396440c558ad1e706f313b19c060
Ole Bak Jensen <obj@trifork.com>
2022-04-19 15:11:54 +0200

    Don't create cra db user

--- compose/database/db/create_db.sql
+++ compose/database/db/create_db.sql
@@ -1,6 +1,3 @@
 CREATE USER 'bemyndigelse'@'%' IDENTIFIED BY '';
 CREATE DATABASE bemyndigelse;
 GRANT ALL PRIVILEGES ON bemyndigelse.* TO 'bemyndigelse'@'%';
-
-CREATE USER 'cra'@'%' IDENTIFIED BY 'cra';
-GRANT ALL PRIVILEGES ON bemyndigelse.* TO 'cra'@'%';
