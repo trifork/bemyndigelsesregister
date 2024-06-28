f40c3bc6dcd7d867619a34b1fe99e35f9a06fb69
Flemming Jørgensen <fjo@trifork.com>
2024-06-28 11:00:00 +0200

    New JTE

--- pipeline_config.groovy
+++ pipeline_config.groovy
@@ -1,7 +1,7 @@
 libraries{
     maven
     docker {
-        output_image = '{"name":"bem","image":"registry.fmk.netic.dk/bemyndigelse/bemyndigelsesregister","user":"bemyndigelsesregister"}'
+        output_images = ['{"name":"bem","image":"registry.fmk.netic.dk/bemyndigelse/bemyndigelsesregister","user":"bemyndigelsesregister"}']
         java_version = 17
     }
     sonarqube_maven
