62eff61ba2228e3adf6a565bb48593aa370e1c3d
Ole Bak Jensen <obj@trifork.com>
2024-11-20 14:43:25 +0100

    add fmk-online profile to pipeline config

--- pipeline_config.groovy
+++ pipeline_config.groovy
@@ -1,4 +1,5 @@
 libraries{
+    settings { profile = 'fmk-online' }
     maven
     docker {
         output_images = '[{"name":"bem","image":"registry.fmk.netic.dk/bemyndigelse/bemyndigelsesregister","user":"bemyndigelsesregister"}]'
