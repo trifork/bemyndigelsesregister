e6dcad4505947e9c9f2e97af3627d4c5091a2e2c
Thomas Alexandersen <thal@trifork.com>
2024-12-05 09:11:18 +0100

    java 21

--- pipeline_config.groovy
+++ pipeline_config.groovy
@@ -3,7 +3,7 @@ libraries{
     maven
     docker {
         output_images = '[{"name":"bem","image":"registry.fmk.netic.dk/bemyndigelse/bemyndigelsesregister","user":"bemyndigelsesregister"}]'
-        java_version = 17
+        java_version = 21
     }
     sonarqube_maven
 }
